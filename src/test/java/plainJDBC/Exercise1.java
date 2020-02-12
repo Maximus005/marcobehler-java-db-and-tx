569c6dcabebe02d320c9b7a0c7605ed78e21b73a
Max_Tarasov <tarasov005@mail.ru>
2020-02-12 22:42:50 +0300

    Initial commit

---
+++ src/test/java/plainJDBC/Exercise1.java
@@ -0,0 +1,12 @@
+package plainJDBC;
+
+import org.junit.Test;
+
+public class Exercise1 {
+
+    @Test
+    public void your_code_goes_here() {
+        // fill in your code here
+        System.out.println("#letsgochamp!");
+    }
+}
